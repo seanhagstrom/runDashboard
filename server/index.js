2d43916b237787eb0b9ddfdf03ae430981d2974a
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-17 12:24:33 -0500

    added public directory and rendered index.html to browser

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const Port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send(`Hello World! This is my runDashboard!`);
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 app.listen(Port, () => {
