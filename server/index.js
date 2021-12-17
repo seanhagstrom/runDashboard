5f8d473e3f099c81818d5a350c2a1914afb1f90c
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-17 12:02:07 -0500

    rendered message from server in browser

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const Port = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.send(`Hello World! This is my runDashboard!`);
+});
+
 app.listen(Port, () => {
   console.log(`runDashboard server is running on port ${Port}!`);
 });
