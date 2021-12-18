64ced023af03516f6cf1166350f213771417fd11
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-18 17:54:28 -0500

    added webpack, babel, morgan, and body parsing middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const Port = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
