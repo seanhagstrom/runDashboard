dd96984342024ca3273db9dad7ec8971af35ea10
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-23 19:57:53 -0500

    added some css

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,8 +2,41 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome home!</h1>
+    <div className='main-container'>
+      <section>
+        <h1>Welcome to the home page!</h1>
+        <div className='flex'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Suspendisse interdum consectetur libero id faucibus nisl tincidunt
+            eget. Neque aliquam vestibulum morbi blandit cursus risus at
+            ultrices. Faucibus nisl tincidunt eget nullam non nisi est.
+            Consequat semper viverra nam libero justo laoreet sit amet cursus.
+            Ut enim blandit volutpat maecenas volutpat. In iaculis nunc sed
+            augue. Enim facilisis gravida neque convallis a cras semper auctor
+            neque. A pellentesque sit amet porttitor. Non arcu risus quis varius
+            quam quisque id diam. Amet consectetur adipiscing elit pellentesque
+            habitant morbi tristique senectus et.
+            <br></br>
+            <br></br>
+            Leo vel fringilla est ullamcorper eget nulla facilisi etiam
+            dignissim. Leo vel fringilla est ullamcorper. Natoque penatibus et
+            magnis dis parturient montes nascetur ridiculus. Enim blandit
+            volutpat maecenas volutpat blandit. Tempus imperdiet nulla malesuada
+            pellentesque elit eget gravida cum. Aenean vel elit scelerisque
+            mauris pellentesque pulvinar pellentesque habitant. Libero id
+            faucibus nisl tincidunt eget nullam non. Rutrum tellus pellentesque
+            eu tincidunt tortor aliquam nulla facilisi. Interdum consectetur
+            libero id faucibus nisl tincidunt eget. Eu volutpat odio facilisis
+            mauris sit amet massa vitae. Consectetur purus ut faucibus pulvinar
+            elementum integer enim neque. Ac orci phasellus egestas tellus
+            rutrum tellus pellentesque eu tincidunt. Etiam erat velit
+            scelerisque in dictum non. Sed euismod nisi porta lorem mollis
+            aliquam ut porttitor.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
