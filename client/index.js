f4fdc96cea659b9ac79d175fabb243649f8adca3
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-03-31 17:50:24 -0400

    added sw.js and registerServiceWorker to boilerplate

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { registerServiceWorker } from '../registerServiceWorker';
+import App from './App';
+// import sw from '../public/sw';
 
+registerServiceWorker();
 ReactDom.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
