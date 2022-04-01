d5528030c094caf1885c14f19015d1080361b3e0
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-04-01 10:06:05 -0400

    added manifest.json file with links to resources for pwa

--- client/index.js
+++ client/index.js
@@ -3,12 +3,12 @@ import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { registerServiceWorker } from '../registerServiceWorker';
 import App from './App';
-// import sw from '../public/sw';
 
-registerServiceWorker();
 ReactDom.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('app')
 );
+
+registerServiceWorker();
--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,10 @@
+// Include your cacheing strategy in this file
+/*** Helpful resources:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+ *
+ * https://web.dev/new-pwa-training/
+ *
+ * https://developer.chrome.com/docs/workbox/
+ */
+
 console.log('Hello from your sw!');
