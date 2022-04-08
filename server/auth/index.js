10f2e42716b65c0c3f1ac2f180f0081926dcf22a
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-04-08 09:29:52 -0400

    basic structure of auth/login route written and returns user based on email

--- server/auth/index.js
+++ server/auth/index.js
@@ -3,6 +3,19 @@ const {
   models: { User },
 } = require('../db');
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    res.send(user);
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      res.status(409).send('User already exists');
+    } else {
+      next(error);
+    }
+  }
+});
 router.post('/signup', async (req, res, next) => {
   try {
     const { email, password } = req.body;
