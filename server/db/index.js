789f3c854721ea4a423ed0d187511d26090c726d
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-04-15 12:11:11 -0400

    added team model and association

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,14 @@
-const db = require("./db");
-const User = require("./models/User");
+const db = require('./db');
+const User = require('./models/User');
+const Team = require('./models/Team');
+
+User.belongsToMany(Team, { through: 'CoachTeam' });
+Team.belongsToMany(User, { through: 'CoachTeam' });
+
 module.exports = {
   db,
   models: {
     User,
+    Team,
   },
 };
